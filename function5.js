dfc0f39b851b2293492378f988e3927c98a803e7
wanko <kouji124@w4.dion.ne.jp>
2020-09-18 02:51:47 +0900

    changed floating point

--- function5.js
+++ function5.js
@@ -2,6 +2,6 @@ function calcTriangle(base, height) {
     return base * height / 2;
 }
 
-let area = calcTriangle(2.32, 6.1);
+let area = calcTriangle(2.32, 6.143);
 
-console.log(`The area is ${area}.`);
+console.log(`The area is ${area.toFixed(2)}.`);
