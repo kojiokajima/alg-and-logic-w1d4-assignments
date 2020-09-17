65ed2e206663679c9707e8e69dbfd3cbc3ca412f
wanko <kouji124@w4.dion.ne.jp>
2020-09-18 02:41:34 +0900

    added invalid session

--- function1.js
+++ function1.js
@@ -1,7 +1,11 @@
 function calcRemainder(num1, num2) {
-    return num1 % num2;
+    if(num1 % 1 !== 0 || num2 % 1 !== 0) {
+        return 'Invalid input. Try again.';
+    } else {
+        return  `The remainder is ${num1 % num2}.`
+    }
 }
 
 let remainder = calcRemainder(10, 3);
 
-console.log(`The remainder is ${remainder}.` );
+console.log(remainder);
