7e96a09e67241539dbd9456ae4cd91fdd7741a1e
wanko <kouji124@w4.dion.ne.jp>
2020-09-18 02:50:22 +0900

    changed some sentences

--- function4.js
+++ function4.js
@@ -1,10 +1,10 @@
 function tempConvert(temp, oriType) {
     if (oriType === 'c') {
-        return `It is ${(1.8 * temp + 32).toFixed(3)} degrees Celsius.` ;
+        return `It is ${(1.8 * temp + 32).toFixed(1)} degrees Fahrenheit.` ;
     } else if (oriType === 'f') {
-        return `It is ${((temp - 32) / 1.8).toFixed(3)} degrees Fahrenheit.`;
+        return `It is ${((temp - 32) / 1.8).toFixed(1)} degrees Celsius.`;
     } else {
-        return 'Invalid input';
+        return 'Invalid input. Try again.';
     }
 }
 
