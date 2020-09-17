c76add2715fee419e81289944ad398937e5e6376
wanko <kouji124@w4.dion.ne.jp>
2020-09-18 02:46:09 +0900

    rechanged result display

--- function2.js
+++ function2.js
@@ -2,6 +2,6 @@ function minutesToSeconds(minutes) {
     return minutes * 60;
 }
 
-let seconds = minutesToSeconds(5.99).toFixed(3);
+let seconds = minutesToSeconds(5.99).toFixed(1);
 
 console.log(`It is ${seconds} seconds.`);
