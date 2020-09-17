4bdb97beece02377bcb36567762d757dc5a83187
wanko <kouji124@w4.dion.ne.jp>
2020-09-18 02:46:57 +0900

    changed result display

--- function3.js
+++ function3.js
@@ -2,6 +2,6 @@ function hoursToSeconds(hours) {
     return hours * 3600;
 }
 
-let seconds = hoursToSeconds(1.2538).toFixed(2);
+let seconds = hoursToSeconds(1.2538).toFixed(3);
 
 console.log(`It is ${seconds} seconds.`);
